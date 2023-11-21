1b9b343a9442369bbd1f23b4abdbffd58de5a3c5
Krier <vinoshan@outlook.com>
2023-11-20 23:14:40 -0500

    Fixed Hero Section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,56 +1,58 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-12 my-4'>
-
-      <div className='col-span-7 place-self-center text-center sm:text-left'>
-        <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl  font-extrabold' >
-          <span className='text-transparent bg-clip-text bg-gradient-to-r from-amber-500 via-yellow-500 to-lime-500 '>Hello! I'm {""}</span>
-
-          <br />
-
+    <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
+      <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
+        <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
+          <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-500">
+            Hello, I'm{" "}
+          </span>{" "}
+          <br></br>
           <TypeAnimation
             sequence={[
-              'Vinoshan Kugendran',
+              "Vinoshan",
               1000,
-              'A Gamer',
+              "Web Developer",
               1000,
-              'A Web Developer',
+              "A Gamer",
               1000,
             ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
           />
-
         </h1>
-        <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus libero perspiciatis earum, obcaecati culpa sit modi enim aspernatur assumenda beatae.
+        <p className="text-[#ADB7BE] mb-6 textl-lg lg:text-xl">
+          Welcome to my world of digital challenges and endless fun!
         </p>
-
         <div>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-amber-500 via-yellow-500 to-lime-500 hover:to-slate-200 text-white'>Hire Me</button>
-          <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-amber-500 via-yellow-500 to-lime-500  hover:bg-slate-800 text-white mt-3'>
-            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV </span>
+          <button className="bg-gradient-to-r from-yellow-400 to-yellow-500 hover:bg-slate-200 text-black px-6 py-3 rounded-full mr-4">
+            Hire Me
+          </button>
+          <button className="m-4 bg-gradient-to-r from-yellow-400 to-yellow-500 px-1 py-1  text-white rounded-full">
+            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Download CV
+            </span>
           </button>
         </div>
       </div>
-
-
-
-      <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-
-        <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[305px] lg:h-[305px] relative'>
-          <Image src='/images/pfp.png'
-            alt='profile picture'
-            width={300} height={300}
-            className='rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' />
+      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="bg-[#181818] lg:w-[350px] lg:h-[350px] w-[250px] h-[250px] rounded-full relative">
+          <Image
+            src="/images/pfp.png"
+            alt="hero image"
+            className=" rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width={300}
+            height={300}
+          />
         </div>
-
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
