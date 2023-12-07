ae8ebc20d95cfa549a384fda0c8639fe8511a2af
Krier <vinoshan@outlook.com>
2023-12-06 23:27:01 -0500

    Bug Fixes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,11 +47,10 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let's Connect
+          Let&apos;s
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          Hey, I'm on the hunt for new gigs or opportunities! Shoot me a message anytime. Got a question or just want to drop a quick hello? I'm all ears – or in this case, all pixels!
+          Hey, I&apos;m on the hunt for new gigs or opportunities! Shoot me a message anytime. Got a question or just want to drop a quick hello? I&apos;m all ears – or in this case, all pixels!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="github.com">
