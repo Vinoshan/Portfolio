02009b5e61016a7718d80652480ff92f75bb97f6
Krier <vinoshan@outlook.com>
2023-12-07 19:56:30 -0500

    Test 2

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import AboutSection from './components/AboutSection'
 import ProjectSection from './components/ProjectSection'
-
+import Footer from './components/Footer'
+import EmailSection from './components/EmailSection'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
@@ -12,7 +13,9 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   )
 }
