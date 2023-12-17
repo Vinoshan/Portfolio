88e066586aab881782f74f9f909aaf193967748d
Krier <vinoshan@outlook.com>
2023-12-17 17:42:37 -0500

    Fixed Mobile Nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] md:bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-20 bg-[#121212] md:bg-opacity-90">
       <div className="flex flex-wrap items-center justify-between mx-auto p-8">
         <Link
           href="/"
